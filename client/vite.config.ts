3a51363815eaab7cd9e3fb1d7d6f83e5c1481ab1
fritzbeing <munkailasule3@gmail.com>
2025-12-29 16:25:19 +0000

    ../D/

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/ghanasupremecases/',
+  base: '/',
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
